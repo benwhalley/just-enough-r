46b05d335620d5a9881cc580a6684842794102b4
benwhalley <ben.whalley@plymouth.ac.uk>
2018-01-29 20:49:49 +0000

    fixed install section

--- docs/requirements.R
+++ docs/requirements.R
@@ -1,33 +1,15 @@
-# This installs all dependencies successfully on OS X and Linux
-# provided you have a working GCC.
-# sudo apt-get install GCC R
-
-dotR <- file.path(Sys.getenv("HOME"), ".R")
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, "Makevars")
-if (!file.exists(M)) file.create(M)
-cat("\nCXXFLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function", 
-    file = M, sep = "\n", append = TRUE)
-
-cat("\nCXXFLAGS+=-flto -ffat-lto-objects  -Wno-unused-local-typedefs", 
-    file = M, sep = "\n", append = TRUE)
-
-Sys.setenv(MAKEFLAGS = "-j4") 
-install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 
 
 # see https://github.com/s-u/PKI/issues/17
 install.packages('PKI',,'http://www.rforge.net/')
 
-# Install other packages I often use
-## Dependencies include reshape2, dplyr
+# Install other packages often used
 pkgs <- c(
   'AER',
   'afex',
   'apa',
   'apaTables',
   'arm', 
-  'bayesplot',
   'blme',
   'bookdown', 
   'brms',
@@ -71,7 +53,6 @@ pkgs <- c(
   'repmis',
   'reshape2',
   'rgl',
-  'rstanarm',
   'rsvg',
   'semPlot',
   'servr',
@@ -87,10 +68,4 @@ install.packages(pkgs)
 
 
 devtools::install_github("ropenscilabs/skimr")
-devtools::install_github("mjskay/tidybayes")
-devtools::install_github("rmcelreath/rethinking")
 devtools::install_github('ralfer/apa_format_and_misc', subdir='apastats')
-
-
-# install dev version otherwise fails on R 3.3.3
-install.packages("MuMIn", repos="http://R-Forge.R-project.org")
--- docs/start_here.md
+++ docs/start_here.md
@@ -35,11 +35,10 @@ The simplest way to get started with RStudio is to get an account on a server in
 
 #### Package dependencies {- #dependencies}
 
-As noted, this guide uses a number of recent R packages to make learning R simpler and more consistent. This requires that the packages are installed first.
 
-To install some of the recommended packages you will need a working C compiler. 
+[This script](requirements.R) installs all dependencies on a recent Linux or Mac system (excluding `rstan` and `rstanarm` which are additionally needed to work through the examples on Baysian estimation, but not the majority of the material).
 
-[This script](requirements.R) installs all dependencies on a recent Linux or Mac system. 
+[This script](install-bayes-tools.R) installs `rstan` and related tools. If you have problems with it however, refer to the latest installation guides for the relevant packages. `rstan` does require you to install a C++ compiler first.
 
 
 
